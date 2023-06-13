06bc16e15a48533c15c68f80db955ce2d0765a8b
Yeva Yesypenko <yeva.yesypenko@gmail.com>
2023-06-13 16:13:59 +0100

    changes2

--- Eve/home.js
+++ Eve/home.js
@@ -15,10 +15,13 @@ hp_text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
 
 
 let aboutThisProject= document.getElementById("aboutThisProject");
-homePage_intro.textContent = "ABOUT THE PROJECT";
+aboutThisProject.textContent = "ABOUT THE PROJECT";
 
-let hp_text = document.getElementById("homePage_text");
-hp_text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+let descriptionOfProject1 = document.getElementById("descriptionOfProject1");
+descriptionOfProject1.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ";
+
+let descriptionOfProject2 = document.getElementById("descriptionOfProject2");
+descriptionOfProject2.textContent = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur";
 
 let colleges= document.getElementById("colleges");
 uni.textContent = "colleges";
