0d489f6971dbb891de7b47c52faab9d2acdb142c
Yeva Yesypenko <yeva.yesypenko@gmail.com>
2023-06-13 14:19:39 +0100

    changes

--- Eve/home.js
+++ Eve/home.js
@@ -7,9 +7,15 @@ let contactHeader = document.getElementById("contact");
 contactHeader.textContent = "Contact us";
 
 
-
 let homePage_intro= document.getElementById("homePage_intro");
 homePage_intro.textContent = "accessable education";
+
+let hp_text = document.getElementById("homePage_text");
+hp_text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+
+let aboutThisProject= document.getElementById("aboutThisProject");
+homePage_intro.textContent = "ABOUT THE PROJECT";
 
 let hp_text = document.getElementById("homePage_text");
 hp_text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
@@ -29,5 +35,5 @@ immigrants.textContent = "information for immigrants";
 
 
 
-footerText = document.getElementById("footerText");
+let footerText = document.getElementById("footerText");
 footerText.textContent = "SYNOPTIC PROJECT. GROUP22 (c)";
